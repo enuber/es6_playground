2c16d465db7b7fb338eb51f5f32c6ab1a7693085
Erik Nuber <erik.nuber@yahoo.com>
2017-05-25 14:36:56 -0700

    filter method

--- app/index.js
+++ app/index.js
@@ -65,3 +65,52 @@ var prices = cars.map(function(car) {
 });
 
 console.log(prices);
+
+
+/** filter helper **/
+
+var products = [
+    { name: 'cucumber', type: 'vegetable', quantity: 0, price: 1 },
+    { name: 'orange', type: 'fruit', quantity: 10, price: 15},
+    { name: 'celery', type: 'vegetable', quantity: 30, price: 9},
+    { name: 'banana', type: 'fruit', quantity: 3, price: 5}
+];
+
+var filteredProducts = []
+
+for (var i=0; i < products.length; i++) {
+    if (products[i].type === 'fruit') {
+        filteredProducts.push(products[i]);
+    }
+}
+
+var returnedStuff = products.filter(function(product) {
+    return product.type === 'fruit';
+});
+
+console.log(filteredProducts);
+console.log(returnedStuff);
+
+//Type is vegetable quantity is greater htan 0, price is less than 10
+
+var multiFilter = products.filter(function(product){
+    return product.type === 'vegetable' && product.quantity > 0 && product.price < 10;
+});
+
+console.log(multiFilter);
+
+var post = {id: 4, title: "New Post"};
+var comments = [
+    {postId: 4, content : "awesome post"},
+    {postId: 3, content : "it was okay"},
+    {postId: 4, content : "neat"}
+];
+
+function commentsForPost(post, comments) {
+    return comments.filter(function (comment) {
+        return comment.postId === post.id;
+    });
+}
+
+console.log(commentsForPost(post, comments));
+
