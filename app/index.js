0d3ffd7b2cd75471930e8dc8180b5f457062a999
Erik Nuber <erik.nuber@yahoo.com>
2017-05-25 14:05:37 -0700

    forEach and map

--- app/index.js
+++ app/index.js
@@ -1,1 +1,67 @@
-console.log('App working: open app/index.js to start exploring ES6');
+
+/** forEach **/
+var colors = ['red', 'blue', 'green'];
+
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+colors.forEach(function(color) {
+    console.log(color);
+});
+
+//Create an array of numbers
+
+var numbers = [1,2,3,4,5];
+
+//Create a variabl to hold the sum
+
+var sum = 0;
+
+//Loop over the array, incrementing the sum variable
+
+numbers.forEach(function(number) {
+    sum += number;
+});
+
+//print the sum variable
+
+console.log(sum);
+
+// can also write it like this
+
+function adder(number) {
+    sum += number;
+}
+
+numbers.forEach(adder);
+
+console.log(sum); //note will be double now because didn't reset the sum
+
+
+/** map helper **/
+
+var mapNumbers = [1,2,3];
+var doubledNumbers = [];
+
+for (var i=0; i< mapNumbers.length; i++) {
+    doubledNumbers.push(numbers[i] * 2);
+}
+
+var doubled = mapNumbers.map(function(number) {
+    return number * 3;
+});
+
+console.log(doubledNumbers, doubled);
+
+
+var cars = [
+    {model: "Buick", price: 'cheap'},
+    {model: "Camaro", price: 'expensive'}
+];
+
+var prices = cars.map(function(car) {
+    return car.price;
+});
+
+console.log(prices);
