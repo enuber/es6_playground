df0b97390eb25b1449269c6a789e86acfa4543ff
Erik Nuber <erik.nuber@yahoo.com>
2017-06-09 17:15:36 -0700

    object litterals lessons done

--- app/index.js
+++ app/index.js
@@ -1,10 +1,36 @@
+// function createBookShop(inventory) {
+//     return {
+//         inventory: inventory,
+//         inventoryValue: function() {
+//             return this.inventory.reduce((total, book)=> total + book.price, 0 );
+//         },
+//         priceForTitle: function(title) {
+//             return this.inventory.find(book => book.title === title).price;
+//         }
+//     }
+// }
+//
+// const inventory = [
+//     { title: 'Harry Potter', price: 10 },
+//     { ttile: "Eloquent JS", price: 15 }
+// ];
+//
+// const bookShop = createBookShop(inventory);
+//
+// console.log(bookShop.inventoryValue());
+// console.log(bookShop.priceForTitle('Harry Potter'));
+
+
+//rewritten with Obj Literals
+//if key and value are exact same name, we can just say it one time. (ie inventory :inventory is just inventory
+//if you have a key value pair this is a function, you can omit the function keyword and the :
 function createBookShop(inventory) {
     return {
-        inventory: inventory,
-        inventoryValue: function() {
+        inventory,
+        inventoryValue() {
             return this.inventory.reduce((total, book)=> total + book.price, 0 );
         },
-        priceForTitle: function(title) {
+        priceForTitle(title) {
             return this.inventory.find(book => book.title === title).price;
         }
     }
@@ -16,3 +42,36 @@ const inventory = [
 ];
 
 const bookShop = createBookShop(inventory);
+
+console.log(bookShop.inventoryValue());
+console.log(bookShop.priceForTitle('Harry Potter'));
+
+
+//another set
+// function saveFile(url, data) {
+//     $.ajax({
+//         method: 'POST',
+//         url: url,
+//         data: data
+//     });
+// }
+//
+// const url = "http://fileupload.com";
+// const data = { color: red };
+//
+// saveFile(url, data);
+
+//again if key and value are same you can just call it one time
+//it is standard to move single key value pairs to the left/top of list
+function saveFile(url, data) {
+    $.ajax({
+        url,
+        data,
+        method: 'POST',
+    });
+}
+
+const url = "http://fileupload.com";
+const data = { color: red };
+
+saveFile(url, data);
