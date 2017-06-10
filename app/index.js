2cf9e9811c71278e7e653695ec22575beb572a7f
Erik Nuber <erik.nuber@yahoo.com>
2017-06-09 20:49:26 -0700

    rest and spread operators lessons done

--- app/index.js
+++ app/index.js
@@ -1,48 +1,61 @@
-//Default Funciton Paramaeters
+//Rest and Spread
 
-function makeAjaxRequest(url, method) {
+function addNums(numbers) {
+    return numbers.reduce((total, number) => {
+        return total + number;
+    },0)
+}
 
-    if (!method) {
-        method = 'Get';
+const a = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(addNums(a));
+
+//this says we don't know how many arguments are coming in, we want to capture them all and put them into
+//an array the ... is a rest operator that allows us to do this.
+
+function addNums1(...numbers) {
+    return numbers.reduce((total, number) => {
+        return total + number;
+    },0)
+}
+
+
+console.log(addNums1(1,2,3,4,5,6,7));
+
+
+const defaultColors = ['red', 'green'];
+const userFavColors = ['orange', 'yellow'];
+
+console.log(defaultColors.concat(userFavColors));
+
+//this is the spread operator,  a new array was created with the [] brackets, we then put inside of it, a reference
+//to existing arrays defaultColors and userFavColors we then added the spread operator which is the ... which means
+//we want to take all of what is in the referenced array and pull them out into the new array. The same process is
+//repeated with all spread operators followed by an array. It removes whatever was in the referenced array and puts it
+//into the new array.
+console.log([...defaultColors, ...userFavColors]);
+//can also add in new elements at the same time...
+console.log(['blue',...defaultColors, 'green', ...userFavColors]);
+
+
+//here we are using both rest and spread. First a list of items of any size is sent in. We check to see
+//if there is milk on the list, if not, we add it to the list of items and return it. Otherwise we just return the list.
+function validateShoppingList(...items) {
+    if (items.indexOf('milk') < 0) {
+        return ['milk', ...items];
     }
-
-    //logic for request
-
+    return items;
 }
 
-makeAjaxRequest('google.com');
-makeAjaxRequest('goole.com', 'GET');
+console.log(validateShoppingList('oranges', 'bread', 'eggs'));
 
 
-//This says if user did not pass in a method argument, it is assgined 'GET' otherwise method is whatever is sent in.
-//if you want it to be undefined specifically you can pass in null instead.
-function makeAjaxRequest(url, method = 'GET') {
-
-    //logic for request
-
-}
-
-makeAjaxRequest('google.com');
-makeAjaxRequest('google.com', 'POST');
-makeAjaxRequest('google.com', null);
-
-
-//__________________________
-
-function User(id) {
-    this.id = id;
-}
-
-function generateID() {
-    return Math.random() * 999999;
-}
-//this makes use of default arguements, if a user isn't passed in, it generates a user to make admin. So can
-//work either way as is called below;
-function createAdminUser(user = new User(generateID())) {
-    user.admin = true;
-    return user;
-}
-
-createAdminUser();
-const user = new User(generateID());
-createAdminUser(user);
+const MathLibrary = {
+    calcProduct (...rest) {
+        console.log('please use multNums instead');
+        return this.multNums(...rest)
+    },
+    multNums(a,b) {
+        return a*b;
+    }
+};
